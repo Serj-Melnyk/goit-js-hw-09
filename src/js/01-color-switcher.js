515ec800308481759d487dd4e277d776af4cfc6a
Serj-Melnyk <SerhiiMelnykGit@gmail.com>
2023-01-18 14:54:21 +0200

    hw-09.js-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let changeColor = '';
 
+buttonStart.addEventListener('click', onClickStart);
+buttonStop.addEventListener('click', onClickStop);
+
+function onClickStart(evt) {
+  buttonStart.setAttribute('disabled', 'disabled');
+
+  changeColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop(evt) {
+  buttonStart.removeAttribute('disabled', 'disabled');
+  clearInterval(changeColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
